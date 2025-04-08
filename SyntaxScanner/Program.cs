b6f8ef4a126d5ba11e54bc186f1bd4235531c555
amo-hada <amirhdv83@gmail.com>
2025-04-08 10:36:10 +0330

    after adding the code into repo

---
+++ SyntaxScanner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+class Program
+{
+    static void Main()
+    {
+        var code = @"
+        public class MyService {
+            public void SayHello() {
+                Console.WriteLine(""Hello, world!"");
+            }
+        }";
+
+        var tree = CSharpSyntaxTree.ParseText(code);
+        var root = tree.GetRoot();
+
+        var method = root.DescendantNodes()
+            .OfType<MethodDeclarationSyntax>()
+            .FirstOrDefault();
+
+        if (method != null)
+        {
+            Console.WriteLine("Method found: " + method.Identifier);
+        }
+        else
+        {
+            Console.WriteLine("No method found.");
+        }
+    }
+}
